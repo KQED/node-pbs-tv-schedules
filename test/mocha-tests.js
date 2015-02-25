db5932970fa352ecf58a8f9a9fb87a97df44a9eb
Kip Gebhardt <kgebhardt@kqed.org>
2015-02-24 17:23:56 -0800

    KIP-1785: Adding ip->zipcode function and unit tests

--- test/mocha-tests.js
+++ test/mocha-tests.js
@@ -1,6 +1,6 @@
 var demand  = require('must'),
     assert  = require('assert'),
-    PBSTvSchedules = require('./../lib/node-pbs-tv-schedules');
+    PBSTvSchedules = require('./../');
 
 var testzip_good = 94110,
     testzip_bad = 11111;
@@ -10,12 +10,14 @@ var api_key = process.env.PBS_TV_SCHEDULES_API_KEY || null;
 PBSApi.set_api_key(api_key);
 console.log("PBSApi.set_api_key(api_key);", api_key);
 describe("PBSApi", function(){
+    this.timeout(7000); // The PBS API is slow.
+
     it("must be creatable", function(){
         PBSApi.must.be.an.instanceof(PBSTvSchedules);
     });
 
     it("should get a list of callsigns for zip " + testzip_good + "  (async)", function(finished){
-        PBSApi.get_stations_by_zip_async(testzip_good, function(err, results){
+        PBSApi.get_callsigns_by_zip_async(testzip_good, function(err, results){
             results.must.be.an.array();
             results.length.must.equal(3);
             finished();
@@ -23,10 +25,7 @@ describe("PBSApi", function(){
     });
 
     it("should get a list of callsigns for zip " + testzip_good + " (promises)", function(finished){
-        this.timeout(3000);
-
-        var testzip = 94110;
-        PBSApi.get_stations_by_zip(testzip_good)
+        PBSApi.get_callsigns_by_zip(testzip_good)
         .then(function(results){
             results.must.be.an.array();
             results.length.must.equal(3);
@@ -35,10 +34,33 @@ describe("PBSApi", function(){
         .done();
     });
 
+    var test_ip = '8.8.8.8',
+        test_zip = '94040';
+    it("should get a zip code for ip address " + test_ip + " (promises)", function(finished){
+        PBSApi.get_zip_from_ip(test_ip)
+        .then(function(results){
+            results.must.be.an.object();
+            results.must.have.keys([ '$type', '$items', '$elements', '$self' ]);
+            results.$items.must.be.an.array();
+            results.$items[0].zipcode.must.be(test_zip);
+            finished();
+        })
+        .done();
+    });
+
+    it("should get a zip code for ip address " + test_ip + " (async)", function(finished){
+        PBSApi.get_zip_from_ip_async(test_ip, function(err, results){
+            results.must.be.an.object();
+            results.must.have.keys([ '$type', '$items', '$elements', '$self' ]);
+            results.$items.must.be.an.array();
+            results.$items[0].zipcode.must.be(test_zip);
+            finished();
+        });
+    });
+
     // **************** These fail without API KEY
     if (PBSApi.api_key) {
         it("should get a list of shows for a callsign (async)", function(finished){
-            this.timeout(3000);
             var program_id = 3190;
             var callsign = "KQED",
                 lc_callsign = callsign.toLowerCase();
@@ -52,7 +74,6 @@ describe("PBSApi", function(){
         });
 
         it("should get a list of shows for a callsign (promises)", function(finished){
-            this.timeout(3000);
             var program_id = 3190;
             var callsign = "KQED",
                 lc_callsign = callsign.toLowerCase();
